282714db98f7b4ff5dcd6c5c2811a383cb1bb1e2
sashka <144311184+iamsashka@users.noreply.github.com>
2024-11-20 13:28:10 +0300

    лалалла

---
+++ app/src/main/java/com/example/paraaaaaa/SecondFragment.java
@@ -0,0 +1,69 @@
+package com.example.paraaaaaa;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.annotation.SuppressLint;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class SecondFragment extends Fragment {
+
+    private TextView countTextView;
+    private int count = 0;
+
+    @SuppressLint("MissingInflatedId")
+    @Nullable
+    @NonNull
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanseState){
+        View view = inflater.inflate(R.layout.second_fragment, container, false);
+        view.setBackgroundResource(R.color.Thistle);
+
+        countTextView = view.findViewById(R.id.countTextView);
+        setHasOptionsMenu(true);
+
+        return view;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.popupmenu_second, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.plus) {
+            count++;
+            countTextView.setText(String.valueOf(count));
+            return true;
+        }
+        else if (item.getItemId() == R.id.minus) {
+            if (count > 0) {
+                count--;
+                countTextView.setText(String.valueOf(count));
+            }
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
+
